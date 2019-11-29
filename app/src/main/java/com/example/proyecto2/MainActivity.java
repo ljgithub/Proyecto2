f2bcb31238b43ad1cb16fac08eef48c3c84d5c90
master <jjmm7@live.com>
2019-11-29 18:42:11 -0500

    Version 1

---
+++ app/src/main/java/com/example/proyecto2/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.proyecto2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void holaMundo(){
+        System.out.println("Hola Pinchi Mundo");
+    }
+
+}
